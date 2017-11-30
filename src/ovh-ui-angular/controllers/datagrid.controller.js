74fa5f88e3cb83ab12d0ff6642b3b607d8c18daf
Kevin Manson <kevin.manson@corp.ovh.com>
2017-11-30 11:42:22 -0500

    feat(datagrid): add controller for datagrid

---
+++ src/ovh-ui-angular/controllers/datagrid.controller.js
@@ -0,0 +1,122 @@
+import _ from "lodash";
+import data from "./datagrid.data.json";
+
+const defaultSliceSize = 50;
+
+export default class {
+    constructor ($scope, $timeout, orderByFilter) {
+        "ngInject";
+
+        this.orderBy = orderByFilter;
+        this.scope = $scope;
+        this.timeout = $timeout;
+    }
+
+    $onInit () {
+        this.trigger = true;
+        this.delay = 0;
+
+        this.timeout(() => {
+            this.data = data;
+
+            this.partialData = _.map(data.slice(0, defaultSliceSize), line => _.pick(line, ["firstName", "lastName"]));
+        }, 1000); // eslint-disable-line no-magic-numbers
+
+        this.label = "value";
+        this.pageLabel = "Page";
+        this.emptyList = [];
+    }
+
+    loadData ({ offset, pageSize, sort, searchText }) {
+        let filteredData;
+        if (searchText) {
+            const regExp = new RegExp(searchText, "i");
+            filteredData = data.filter(row => regExp.test(row.firstName) ||
+                regExp.test(row.lastName) ||
+                regExp.test(row.email) ||
+                regExp.test(row.phone) ||
+                regExp.test(row.birth));
+        } else {
+            filteredData = data;
+        }
+
+        const sortedData = sort.property ? this.orderBy(filteredData, sort.property, sort.dir === -1) : data;
+        const page = sortedData.slice(offset, offset + pageSize);
+
+        // AngularJS independent logic
+        return new Promise(resolve => {
+            setTimeout(() => {
+                resolve({
+                    data: page,
+                    meta: {
+                        currentOffset: offset,
+                        pageCount: Math.ceil(filteredData.length / pageSize),
+                        totalCount: filteredData.length
+                    }
+                });
+            }, this.delay);
+        });
+    }
+
+    loadPartialData ({ offset, pageSize, sort, searchText }) {
+        let filteredData;
+        if (searchText) {
+            const regExp = new RegExp(searchText, "i");
+            filteredData = data.filter(row => regExp.test(row.firstName) ||
+                regExp.test(row.lastName) ||
+                regExp.test(row.email) ||
+                regExp.test(row.phone) ||
+                regExp.test(row.birth));
+        } else {
+            filteredData = data;
+        }
+
+        const sortedData = sort.property ? this.orderBy(filteredData, sort.property, sort.dir === -1) : data;
+        const page = sortedData.slice(offset, offset + pageSize);
+
+        // AngularJS independent logic
+        return new Promise(resolve => {
+            setTimeout(() => {
+                resolve({
+                    data: _.map(page, line => _.pick(line, ["firstName", "lastName"])),
+                    meta: {
+                        currentOffset: offset,
+                        pageCount: Math.ceil(filteredData.length / pageSize),
+                        totalCount: filteredData.length,
+                        pageSize
+                    }
+                });
+            }, this.delay);
+        });
+    }
+
+    loadRow ({ firstName, lastName }) { // eslint-disable-line
+        return new Promise(resolve => {
+            setTimeout(() => {
+                resolve(_.find(data, { firstName, lastName }));
+            }, 500 + 1000 * Math.random()); // eslint-disable-line
+        });
+    }
+
+    onSearchText (id) {
+        this.scope.$broadcast(`oui-table:${id}:refresh`, {
+            searchText: this.searchText
+        });
+    }
+
+    addElement () {
+        this.emptyList.push(data[this.emptyList.length]);
+    }
+
+    redraw () {
+        this.trigger = false;
+        this.timeout(() => {
+            this.trigger = true;
+        });
+    }
+
+    changeDataOnFirstLine () {
+        this.data[0].firstName = "TEST";
+        this.data[0].lastName = "TEST";
+    }
+}
--- src/ovh-ui-angular/controllers/index.js
+++ src/ovh-ui-angular/controllers/index.js
@@ -1,6 +1,8 @@
+import datagridController from "./datagrid.controller";
 import paginationController from "./pagination.controller";
 import showCurrentValueInPopupController from "./show-current-value-in-popup.controller";
 
 angular.module("ovh-ui-kit-documentation")
+    .controller("DatagridCtrl", datagridController)
     .controller("PaginationCtrl", paginationController)
     .controller("ShowCurrentValueInPopupCtrl", showCurrentValueInPopupController);
