1e4ce36917251bad10d1cecf8da44a45acc2eeab
Jimmy Fortin <jimmy.fortin@corp.ovh.com>
2017-12-15 08:43:51 -0500

    refactor(oui-datagrid): remove unused stuff
    Signed-off-by: Jimmy Fortin <jimmy.fortin@corp.ovh.com>

--- src/ovh-ui-angular/controllers/datagrid.controller.js
+++ src/ovh-ui-angular/controllers/datagrid.controller.js
@@ -1,20 +1,18 @@
 import _ from "lodash";
-import data from "./datagrid.data.json";
+import data from "ovh-ui-angular/packages/oui-datagrid/src/index.spec.data.json";
 
 const defaultSliceSize = 50;
 
 /* eslint-disable class-methods-use-this */
 export default class {
-    constructor ($scope, $timeout, orderByFilter) {
+    constructor ($timeout, orderByFilter) {
         "ngInject";
 
         this.orderBy = orderByFilter;
-        this.scope = $scope;
         this.timeout = $timeout;
     }
 
     $onInit () {
-        this.trigger = true;
         this.delay = 0;
 
         this.timeout(() => {
@@ -24,39 +22,6 @@ export default class {
         }, 1000); // eslint-disable-line no-magic-numbers
 
         this.label = "value";
-        this.pageLabel = "Page";
-        this.emptyList = [];
-    }
-
-    loadData ({ offset, pageSize, sort, searchText }) {
-        let filteredData;
-        if (searchText) {
-            const regExp = new RegExp(searchText, "i");
-            filteredData = data.filter(row => regExp.test(row.firstName) ||
-                regExp.test(row.lastName) ||
-                regExp.test(row.email) ||
-                regExp.test(row.phone) ||
-                regExp.test(row.birth));
-        } else {
-            filteredData = data;
-        }
-
-        const sortedData = sort.property ? this.orderBy(filteredData, sort.property, sort.dir === -1) : data;
-        const page = sortedData.slice(offset, offset + pageSize);
-
-        // AngularJS independent logic
-        return new Promise(resolve => {
-            setTimeout(() => {
-                resolve({
-                    data: page,
-                    meta: {
-                        currentOffset: offset,
-                        pageCount: Math.ceil(filteredData.length / pageSize),
-                        totalCount: filteredData.length
-                    }
-                });
-            }, this.delay);
-        });
     }
 
     loadPartialData ({ offset, pageSize, sort, searchText }) {
@@ -97,27 +62,5 @@ export default class {
                 resolve(_.find(data, { firstName, lastName }));
             }, 500 + 1000 * Math.random()); // eslint-disable-line
         });
-    }
-
-    onSearchText (id) {
-        this.scope.$broadcast(`oui-table:${id}:refresh`, {
-            searchText: this.searchText
-        });
-    }
-
-    addElement () {
-        this.emptyList.push(data[this.emptyList.length]);
-    }
-
-    redraw () {
-        this.trigger = false;
-        this.timeout(() => {
-            this.trigger = true;
-        });
-    }
-
-    changeDataOnFirstLine () {
-        this.data[0].firstName = "TEST";
-        this.data[0].lastName = "TEST";
     }
 }
