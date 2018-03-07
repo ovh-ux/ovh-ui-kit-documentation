18a5a3dd739fabc2982fcfda0860a15126c42647
Axel Peter <axel.peter@corp.ovh.com>
2018-03-07 17:35:54 +0100

    feat(oui-chips): add oui-chips component

---
+++ src/ovh-ui-angular/controllers/chips.controller.js
@@ -0,0 +1,19 @@
+import data from "ovh-ui-angular/packages/oui-chips/src/index.spec.data.json";
+
+export default class {
+    $onInit () {
+        this.items = angular.copy(data.items);
+        this.itemsValue = angular.copy(data.items);
+    }
+
+    addItem () {
+        this.itemsValue.push({
+            title: "Lorem ipsum",
+            foo: "bar"
+        });
+    }
+
+    onRemove (items) {
+        this.changedValue = angular.copy(items);
+    }
+}
