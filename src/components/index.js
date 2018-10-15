b3f256a76b5e01cbbc64042dc94eb091e112140a
CDS Agent <axel.peter@corp.ovh.com>
2018-10-15 11:07:28 +0000

    Deployment 873 - documentation site update

--- src/components/index.js
+++
@@ -1,5 +0,0 @@
-import IndeterminateDirective from "./indeterminate.directive";
-
-export default angular
-    .module("ovh-ui-kit-documentation-components", [])
-    .directive("indeterminate", IndeterminateDirective);
